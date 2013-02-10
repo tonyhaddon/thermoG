1e437e0dd6bbd22ba0f81160a11baaf2022e8f5e
Tony Haddon <tony.haddon@gmail.com>
2013-02-10 10:32:49 +0000

    Separated out secrets and URLs

--- arduino/ThermometerHTTPGet/ThermometerHTTPGet.ino
+++ arduino/ThermometerHTTPGet/ThermometerHTTPGet.ino
@@ -2,12 +2,14 @@
 #include <WiFly.h>
 
 #include "Credentials.h"
+#include "url_secret.h"
 //int dispPin; 
 /*
 * setup() – this function runs once when you turn your Arduino on
 * We the three control pins to outputs
 */
 int temperaturePin = 0;
+float lastTemp = 0.0;
 
 void setup()
 {
@@ -35,15 +37,20 @@ void loop()                     // run over and over again
  float temperature = getVoltage(temperaturePin);  //getting the voltage reading from the temperature sensor
  temperature = (temperature - .5) * 100;          //converting from 10 mv per degree wit 500 mV offset
                                                   //to degrees ((volatge - 500mV) times 100)
-
-
+if (temperature != lastTemp)
+{
   //Serial.println("connecting...");
   WiFlyClient client("www.tonyhaddon.com", 80);
   if (client.connect()) {
     Serial.println("connected");
-    client.print("GET http://www.tonyhaddon.com/thermog-tweet/twitteroauth-master/tweet.php?tmp=");
+    client.print("GET ");
+    client.print(php_script_url);
+    client.print("?tmp=");
     client.print(temperature);
-    client.print("&scrt=tellno1 HTTP/1.0");
+    client.print("&scrt=");
+    client.print(secret1);
+    client.print(secret2);
+    client.print(" HTTP/1.0");
     client.print("\r\n");
     client.println("From: tony@tonyhaddon.com");
     client.println("User-Agent: ThermoG/0.1");
@@ -54,15 +61,12 @@ void loop()                     // run over and over again
   } else {
     Serial.println("connection failed");
   }
-  
-  while(client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-                                             
- 
- delay(600000); // Ten Minutes
-//delay(60000); // One Minutes
+                                           
+ lastTemp = temperature;
+}
+ // delay(600000); // Ten Minutes
+ //delay(60000); // One Minutes
+ delay(180000); // Two Minutes
  //delay(10000); // Ten Seconds
 }
 
---
+++ arduino/ThermometerHTTPGet/url_secret.sample.h
@@ -0,0 +1,9 @@
+#ifndef __URL_SECRET__
+#define __URL_SECRET__
+
+//	2secret parameters
+char php_script_url[] = ""; //Location of your tweet.php script
+//	These 2 secrets will be combined to offer a little 
+char secret1[] = "";
+char secret2[] = "";
+#endif
